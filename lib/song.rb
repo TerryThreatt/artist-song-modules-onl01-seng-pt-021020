deb54afb757f71a45b726157e182443008ff75f8
Terry Threatt <terry.threatt@gmail.com>
2020-03-15 17:59:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
 require 'pry'
 
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
@@ -11,9 +13,6 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
   def self.all
     @@songs
